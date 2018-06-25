83dad217dfac26a46165f3e053310f0085f3d1f6
Robin Heller <robin_heller@ymail.com>
2018-06-25 23:43:22 +0200

    more fixes, cleanup

--- assets/js/moduleAnimations.js
+++ assets/js/moduleAnimations.js
@@ -1,9 +1,46 @@
 $(document).ready(function() {
 // @TODO fix this to work with delegated events as to also catch events for dynamically created modules…
 // use this as a starting point:
-$('body').on('click', '.Verlaufsplan', function(e) {
-    alert('open module description');
+$('.Verlaufsplan').on('click', '.modul .button', function(e) {
+    let eventTarget = $(e.target).parents('.modul').first();
+    let moduleId = parseInt($(eventTarget).data('moduleid'))
+    let moduleData = getModuleInfo(moduleId)
+
+    loadModuleInfoIntoPopup(moduleData);
+
+    $(eventTarget).css("z-index","1");
+    //create a timeline for the current box
+    var tl = new TimelineLite({
+        paused: true
+    });
+    var popup = $(".popup");
+    tl.to($(eventTarget), 1, {
+        rotationY: 90,
+        x: (viewportWidth / 2 - $(eventTarget)[0].getBoundingClientRect().left - $(eventTarget).width() / 2),
+        y: (viewportHeight / 2 - $(eventTarget)[0].getBoundingClientRect().top - $(eventTarget).height() / 2),
+    })
+    .to(popup, 0.8, {
+        x: (viewportWidth / 2 - popup.width() / 2),
+        y: (viewportHeight / 2  - popup.height() / 2),
+        scaleX: 1,
+        scaleY: 1,
+        rotationY: 0,
+        autoAlpha: true
+    }, "-=0.3")
+    .to($(".overlay"), 0.8, {
+        autoAlpha: true
+    }, "-=1");
+
+    eventTarget.animation = tl;
+    $(eventTarget).css("z-index","8");
+    eventTarget.animation.play();
+
+    $(".popup").click(function(event) {
+        eventTarget.animation.reverse();
+    });
 })
+
+
 modules = $(".modul");
 console.log(modules);
 sortByX = function(a, b) {
@@ -32,50 +69,10 @@ TweenMax.set($(".popup"), {
 
 viewportWidth = $(window).width();
 viewportHeight = $(window).height();
-
-// Modul Flip Description Animation
-$(".modul").each(function() {
-    $(this).css("z-index","1");
-    //create a timeline for each box
-    var tl = new TimelineLite({
-        paused: true
-    });
-    var popup = $(".popup");
-    tl.to($(this), 1, {
-        rotationY: 90,
-        x: (viewportWidth / 2 - $(this)[0].getBoundingClientRect().left - $(this).width() / 2),
-        y: (viewportHeight / 2 - $(this)[0].getBoundingClientRect().top - $(this).height() / 2),
-    })
-        .to(popup, 0.8, {
-            x: (viewportWidth / 2 - popup.width() / 2),
-            y: (viewportHeight / 2  - popup.height() / 2),
-            scaleX: 1,
-            scaleY: 1,
-            rotationY: 0,
-            autoAlpha: true
-        }, "-=0.3")
-        .to($(".overlay"), 0.8, {
-            autoAlpha: true
-        }, "-=1");
-
-    this.animation = tl;
-
-    $(".modul").click(function(event) {
-        $(this).css("z-index","8");
-        this.animation.play();
-        //set current animation to this box's animtion
-        currentAnimation = this.animation;
-
-    });
-
-    $(".popup").click(function(event) {
-        if (currentAnimation) {
-            //if there is a currentAnimation reverse it
-            currentAnimation.reverse();
-        }
-
-    });
-
-
 });
-});
+
+loadModuleInfoIntoPopup = (moduleData) => {
+    for (var key in moduleData) {
+        $('.popup #module-' + key).html(moduleData[key]);
+    }
+}
